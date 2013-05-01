af63984ccff8db6975d68db359981da159b7cbf1
Ben Hollis <ben@benhollis.net>
2013-04-30 19:32:15 -0700

    Add a version of middleman-livereload for the new 3.1 extension mechanism. Incidentally fixes #15

--- lib/middleman-livereload.rb
+++ lib/middleman-livereload.rb
@@ -2,6 +2,11 @@ require "middleman-core"
 require "middleman-livereload/version"
   
 ::Middleman::Extensions.register(:livereload) do
-  require "middleman-livereload/extension"
-  ::Middleman::LiveReload
+  if defined?(::Middleman::Extension)
+    require "middleman-livereload/extension_3_1"
+    ::Middleman::LiveReloadExtension
+  else
+    require "middleman-livereload/extension_3_0"
+    ::Middleman::LiveReload
+  end
 end
--- lib/middleman-livereload/version.rb
+++ lib/middleman-livereload/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   module LiveReload
-    VERSION = "3.0.1"
+    VERSION = "3.1.0"
   end
 end
