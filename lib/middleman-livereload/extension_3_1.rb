5f0fac94a217504c1e2c657056a98de7dfbb410f
Ben Hollis <ben@benhollis.net>
2014-03-18 21:51:29 -0700

    Clean up middleman-livereload, use the correct file ignore API, remove grace_period hack and other nastiness.

--- lib/middleman-livereload/extension_3_1.rb
+++ lib/middleman-livereload/extension_3_1.rb
@@ -7,17 +7,15 @@ module Middleman
     option :port, '35729', 'Port to bind the LiveReload API server to'
     option :apply_js_live, true, 'Apply JS changes live, without reloading'
     option :apply_css_live, true, 'Apply CSS changes live, without reloading'
-    option :grace_period, 0, 'Time (in seconds) to wait before reloading'
     option :no_swf, false, 'Disable Flash WebSocket polyfill for browsers that support native WebSockets'
 
     def initialize(app, options_hash={}, &block)
       super
 
-      return if app.environment != :development
+      return unless app.environment == :development
 
       @reactor = nil
 
-      grace_period = options.grace_period
       port = options.port.to_i
       host = options.host
       no_swf = options.no_swf
@@ -31,28 +29,27 @@ module Middleman
         end
 
         files.changed do |file|
-          next if ignore_manager.ignored?(file)
+          next if files.send(:ignored?, file)
 
-          sleep(grace_period) if grace_period > 0
-          sitemap.ensure_resource_list_updated!
-          puts "Changed! #{file}"
+          logger.debug "LiveReload: File changed - #{file}"
 
-          begin
-            file_url = sitemap.file_to_path(file)
+          reload_path = "#{Dir.pwd}/#{file}"
+
+          file_url = sitemap.file_to_path(file)
+          if file_url
             file_resource = sitemap.find_resource_by_path(file_url)
-            reload_path = file_resource.destination_path
-          rescue
-            reload_path = "#{Dir.pwd}/#{file}"
+            if file_resource
+              reload_path = file_resource.url
+            end
           end
+
           @reactor.reload_browser(reload_path)
         end
 
         files.deleted do |file|
-          next if ignore_manager.ignored?(file)
+          next if files.send(:ignored?, file)
 
-          sleep(grace_period) if grace_period > 0
-          sitemap.ensure_resource_list_updated!
-          puts "Deleted! #{file}"
+          logger.debug "LiveReload: File deleted - #{file}"
 
           @reactor.reload_browser("#{Dir.pwd}/#{file}")
         end
